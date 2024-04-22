54fedb682f313eee4dc69d2b38171a7450e53f82
salif95 <aboubacarsalif.tapsoba@gmail.com>
2024-04-22 05:18:42 -0400

    Initialisation Ressources_Humaines

---
+++ main.py
@@ -0,0 +1,8 @@
+ # Import necessary libraries
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import pandas as pd
+import joblib  # Import joblib for model persistence
